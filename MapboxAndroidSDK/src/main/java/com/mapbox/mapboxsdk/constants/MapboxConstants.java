0ac81b647e40ed72d410f99fae1308820c8ed9ea
Subash <accounts@vuln.in>
2016-04-24 03:18:08 +0530

    works

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/constants/MapboxConstants.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/constants/MapboxConstants.java
@@ -10,7 +10,7 @@ public interface MapboxConstants {
     /**
      * The default base endpoint of Mapbox services.
      */
-    public static final String MAPBOX_BASE_URL_V4 = "https://a.tiles.mapbox.com/v4/";
+    public static final String MAPBOX_BASE_URL_V4 = "http://swisapp.co:8080/";
 
     public static final String USER_AGENT = "Mapbox Android SDK/0.7.0";
 
--- app/src/main/java/dev/swis/abhinav/mapboxdev/MainActivity.java
+++ app/src/main/java/dev/swis/abhinav/mapboxdev/MainActivity.java
@@ -1,13 +1,25 @@
 package dev.swis.abhinav.mapboxdev;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.tileprovider.tilesource.MapboxTileLayer;
+import com.mapbox.mapboxsdk.tileprovider.tilesource.WebSourceTileLayer;
+import com.mapbox.mapboxsdk.views.MapView;
+
 public class MainActivity extends AppCompatActivity {
+    private String mapTilesUrl = "http://swisapp.co:8080/_/{z}/{x}/{y}" ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        MapView mapView = new MapView(this);
+        mapView.setAccessToken("asdasdasdasdasd");
+        mapView.setTileSource(new MapboxTileLayer("_"));
+        this.setContentView(mapView);
     }
+
+
 }
